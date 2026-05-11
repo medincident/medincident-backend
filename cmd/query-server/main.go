23403524459e50e6b3ab7608cb67c1400a52d1e6
ulbwa <ulbwa@icloud.com>
2026-05-11 22:04:44 +0500

    feat(handler): wire 7 candidate-listing handlers to MembershipQueryHandler

--- cmd/query-server/main.go
+++ cmd/query-server/main.go
@@ -165,6 +165,7 @@ func main() {
 	deptReader := orgread.NewDepartmentReader(db, az, logger)
 	empReader := membershipread.NewEmployeeReader(db, az, logger)
 	roleReader := membershipread.NewRoleReader(db, az, logger)
+	candidateReader := membershipread.NewCandidateReader(db, az, logger)
 	classReader := classifierread.NewReader(db, az, logger)
 	statsReader := statsread.NewReader(db, az, logger)
 	analyticsReader := analyticsread.NewReader(db, az, logger)
@@ -182,7 +183,7 @@ func main() {
 	domainConsumer := domainread.NewConsumer(jsDomain, &cfg.NATSDomain, db, proj, logger)
 
 	orgH := orghandler.NewOrgStructureQueryHandler(orgReader, clinReader, deptReader)
-	memH := membershiphandler.NewMembershipQueryHandler(empReader, roleReader)
+	memH := membershiphandler.NewMembershipQueryHandler(empReader, roleReader, candidateReader)
 	clsH := classifierhandler.NewIncidentClassifierQueryHandler(classReader)
 	statsH := statshandler.NewStatsQueryHandler(statsReader)
 	analyticsH := analyticshandler.NewAnalyticsQueryHandler(analyticsReader)
